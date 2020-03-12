ff72484035f4d907927f7feb9a53d2b0aa285ff6
Quetzal Bradley <qbradley@qbradley.com>
2020-03-12 02:08:15 -0700

    implement range scanning (#3)

--- src/sorta.nim
+++ src/sorta.nim
@@ -37,7 +37,12 @@ type
     root: Node[Key, Val]
     entries: int # total number of entries in the tree
 
+  CursorPosition[Key, Val] = tuple
+    ## Index into the sorted table allowing access to either the key or the value.
+    node: Node[Key, Val]
+    entry: int
 
+  Cursor[Key, Val] = seq[CursorPosition[Key, Val]]
 
 template leq(a, b): bool = cmp(a, b) <= 0
 template eq(a, b): bool = cmp(a, b) == 0
@@ -54,7 +59,6 @@ proc binarySearch[Key, Val](x: Key; a: Node[Key, Val]): int {.inline.} =
     else:
       l = i+1
   return r
-
 
 proc initSortedTable*[Key, Val](): SortedTable[Key, Val] =
   ## Creates a new empty SortedTable.
@@ -409,45 +413,79 @@ proc `$`*[Key, Val](b: SortedTable[Key, Val]): string =
 
 proc len*[Key, Val](b: SortedTable[Key, Val]): int {.inline.} = b.entries
 
+proc key[Key, Val](position: CursorPosition[Key, Val]): Key =
+  ## Return the key for a given cursor position.
+  position.node.e[position.entry].key
 
-iterator entries[Key, Val](b: SortedTable[Key, Val]): Entry[Key, Val] =
-  template populateWithLeftmostChildren(stack, currentNode) =
-    while not currentNode.isNil:
-      for i in countdown(currentNode.m-1, 0):
-        stack.add(currentNode.e[i])
-      currentNode = currentNode.p0
+proc val[Key, Val](position: CursorPosition[Key, Val]): Val =
+  ## Return the value for a given cursor position.
+  position.node.e[position.entry].val
 
-  var
-    root = b.root
-    currentEntry: Entry[Key, Val]
-    stack = newSeq[Entry[Key, Val]]()
+proc mval[Key, Val](position: CursorPosition[Key, Val]): var Val =
+  ## Returns a reference to the value for a given cursor position.
+  position.node.e[position.entry].val
 
-  stack.populateWithLeftmostChildren(root)
-  while stack.len > 0:
-    currentEntry = stack.pop()
-    yield currentEntry
-    stack.populateWithLeftmostChildren(currentEntry.p)
+proc search[Key, Val](b: SortedTable[Key, Val], key: Key): Cursor[Key, Val] =
+  ## Calculates the cursor pointing to the given key.
+  var a = b.root
+  while not a.isNil:
+    var r = binarySearch(key, a)
+    if r < a.m:
+      result.add((a, r))
+      if eq(key, a.e[r].key):
+        break
+    a = if r == 0: a.p0 else: a.e[r-1].p
+  # add a dummy entry for first next call
+  result.add((nil, 0))
 
+proc current[Key, Val](cursor: Cursor[Key, Val]): CursorPosition[Key, Val] =
+  ## Returns the current position of a cursor.
+  ## This call is only valid if cursor.next previously returned true.
+  cursor[^1]
 
-iterator mentries[Key, Val](b: var SortedTable[Key, Val]): var Entry[Key, Val] =
-  template populateWithLeftmostChildren(stack, cntNode) =
-    var currentNode = cntNode
-    while not currentNode[].isNil:
-      for i in countdown(currentNode[].m-1, 0):
-        stack.add(addr currentNode[].e[i])
-      currentNode = addr currentNode[].p0
+proc next[Key, Val](cursor: var Cursor[Key, Val]): bool =
+  ## Moves the cursor forward returning true if cursor.current is now valid.
+  ## Never call current after next returns false.
+  var (node, oldEntry) = cursor.pop()
+  if not node.isNil:
+    var newEntry = oldEntry + 1
+    if newEntry < node.m:
+        cursor.add((node, newEntry))
+    var child = node.e[oldEntry].p
+    if not child.isNil:
+      while not child.isNil:
+        cursor.add((child, 0))
+        child = child.p0
+  return cursor.len > 0 and cursor.current.node.m > 0
 
-  var
-    root = unsafeAddr b.root
-    currentEntry: ptr Entry[Key, Val]
-    stack = newSeq[ptr Entry[Key, Val]]()
+proc cursorFromStart[Key, Val](b: SortedTable[Key, Val]): Cursor[Key, Val] =
+  result = @[]
+  var a = b.root
+  while not a.isNil:
+    result.add((a, 0))
+    a = a.p0
+  result.add((nil, 0))
 
-  stack.populateWithLeftmostChildren(root)
-  while stack.len > 0:
-    currentEntry = stack.pop()
-    yield currentEntry[]
-    stack.populateWithLeftmostChildren(addr currentEntry[].p)
+iterator pairsFrom*[Key, Val](b: SortedTable[Key, Val], fromKey: Key): tuple[key: Key, val: Val] =
+  ## Iterates the sorted table from the given key to the end.
+  var cursor = b.search(fromKey)
+  while cursor.next:
+    let position = cursor.current
+    yield (position.key, position.val)
 
+iterator pairsBetween*[Key, Val](b: SortedTable[Key, Val], fromKey: Key, toKey: Key): tuple[key: Key, val: Val] =
+  ## Iterates the sorted table from fromKey to toKey inclusive.
+  var cursor = b.search(fromKey)
+  while cursor.next:
+    let position = cursor.current
+    if not leq(position.key, toKey):
+      break
+    yield (position.key, position.val)
+
+iterator entries[Key, Val](b: SortedTable[Key, Val]): CursorPosition[Key, Val] =
+  var cursor = b.cursorFromStart
+  while cursor.next:
+    yield cursor.current
 
 iterator keys*[Key, Val](b: SortedTable[Key, Val]): Key =
   ## Iterates over all the keys in the table `b`.
@@ -462,8 +500,8 @@ iterator values*[Key, Val](b: SortedTable[Key, Val]): Val =
 iterator mvalues*[Key, Val](b: var SortedTable[Key, Val]): var Val =
   ## Iterates over all the values in the table `b`.
   ## The values can be modified.
-  for e in mentries(b):
-    yield e.val
+  for e in entries(b):
+    yield e.mval
 
 iterator pairs*[Key, Val](b: SortedTable[Key, Val]): (Key, Val) =
   ## Iterates over all `(key, value)` pairs in the table `b`.
@@ -473,8 +511,8 @@ iterator pairs*[Key, Val](b: SortedTable[Key, Val]): (Key, Val) =
 iterator mpairs*[Key, Val](b: var SortedTable[Key, Val]): (Key, var Val) =
   ## Iterates over all `(key, value)` pairs in the table `b`.
   ## The values can be modified.
-  for e in mentries(b):
-    yield (e.key, e.val)
+  for e in entries(b):
+    yield (e.key, e.mval)
 
 
 proc `==`*[Key, Val](a, b: SortedTable[Key, Val]): bool =
@@ -493,7 +531,7 @@ proc `==`*[Key, Val](a, b: SortedTable[Key, Val]): bool =
 
 
 when isMainModule:
-  import random, tables
+  import random, tables, sequtils
 
   proc main =
     var st = initSortedTable[string, string]()
@@ -522,6 +560,41 @@ when isMainModule:
     assert st.getOrDefault("www.ebay.com") == "66.135.192.87"
     assert st.getOrDefault("www.dell.com") == "143.166.224.230"
     assert(st.entries == 16)
+
+    proc collect_from_keys(from_key: string): seq[string] =
+      result = @[]
+      for position in st.pairsFrom(from_key):
+        result.add(position.key)
+
+    var keys = toSeq(st.keys())
+    assert keys.len == 16
+
+    for i in 0..<keys.len:
+      let key = keys[i]
+      assert collect_from_keys(key) == keys[i..<keys.len]
+
+      let lesskey = keys[i][0..^2]
+      assert collect_from_keys(lesskey) == keys[i..<keys.len]
+
+      let morekey = keys[i] & "_"
+      assert collect_from_keys(morekey) == keys[i+1..<keys.len]
+
+    proc collect_keys(from_key: string, to_key: string): seq[string] =
+      result = @[]
+      for position in st.pairsBetween(from_key, to_key):
+        result.add(position.key)
+
+    for i in 0..<keys.len:
+      let from_key = keys[i]
+      for j in i..<keys.len:
+        let to_key = keys[j]
+        assert collect_keys(from_key, to_key) == keys[i..j]
+
+        let to_lesskey = to_key[0..^2]
+        assert collect_keys(from_key, to_lesskey) == keys[i..<j]
+
+        let to_morekey = to_key & "_"
+        assert collect_keys(from_key, to_morekey) == keys[i..j]
 
     for k, v in st:
       echo k, ": ", v
@@ -554,4 +627,21 @@ when isMainModule:
         doAssert $k == v
         doAssert b2.getOrDefault(k) == $k
 
+    # ensure first time cursor.next is called on tree with an empty root node it returns false
+    var b3 = initSortedTable[int, string]()
+    var cursor = b3.cursorFromStart
+    assert not cursor.next
+
+    # check that mvalues works
+    var mutate_st = initSortedTable[string, string]()
+    mutate_st["a"] = "one"
+    for val in mutate_st.mvalues:
+      val = "two"
+    doAssert mutate_st["a"] == "two"
+
+    # check again with mpairs
+    for key, val in mutate_st.mpairs:
+      val = "three"
+    doAssert mutate_st["a"] == "three"
+  
   main()
